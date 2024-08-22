f248dafcd84cdf8048aaef90c1f8c78b5a365d0b
Maher352 <maher.ad352@gmail.com>
2024-08-22 22:37:33 +0300

    Add new Python File: saving_data.py

---
+++ Project/saving_data.py
@@ -0,0 +1,19 @@
+# saving_data.py
+
+import os
+from shutil import copy2
+
+# Function to save data to train and validate folders
+def save_data(images_dir, bboxes_dir, output_dir, split_indices, split_name):
+    split_image_dir = os.path.join(output_dir, split_name, 'images')
+    split_bbox_dir = os.path.join(output_dir, split_name, 'labels')
+
+    os.makedirs(split_image_dir, exist_ok=True)
+    os.makedirs(split_bbox_dir, exist_ok=True)
+
+    for file_index in split_indices:
+        image_filename = f"rgb_{file_index}.png"
+        bbox_filename = f"bounding_box_2d_tight_{file_index}.npy"
+
+        copy2(os.path.join(images_dir, image_filename), os.path.join(split_image_dir, image_filename))
+        copy2(os.path.join(bboxes_dir, bbox_filename), os.path.join(split_bbox_dir, bbox_filename))
